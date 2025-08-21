1e1b174a446599883403208af21e0248f6cdd05c
marzz <eguonzy@yahoo.com>
2025-08-21 01:10:29 +0100

    second commit

--- app/api/products/[query]/route.ts
+++ app/api/products/[query]/route.ts
@@ -1,12 +1,19 @@
+import dbConnect from "@/app/lib/db";
 import { getProductsByQuery } from "@/app/services/product.service";
+import logger from "@/app/utils/logger";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, params: { query: string }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { query: string } }
+) {
   try {
-    const query = (await params).query;
+    await dbConnect();
+    const query = params.query;
     const products = await getProductsByQuery(query ?? undefined); // Assuming this function fetches all products
     return NextResponse.json({ products });
   } catch (error) {
+    logger.error("Error fetching products:" + error);
     return NextResponse.json({ error: error }, { status: 500 });
   }
 }
